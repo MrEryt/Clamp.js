891018d5d688e36d1b67f7bc93cbf9b45a605e51
MrEryt <138548170+MrEryt@users.noreply.github.com>
2023-09-13 12:57:55 +0600

    Update clamp.js

--- clamp.js
+++ clamp.js
@@ -1,48 +1,53 @@
 let ClampInner = {
-        constructor: function (wrapperSelector) {
-            this.$wrapperSelector = $(wrapperSelector);
-            ClampInner.calcClamp();
-        },
-        addClampStyle: function (element, resultCountLines) {
-            element.style.overflow = 'hidden';
-            element.style.textOverflow = 'ellipsis';
-            element.style.webkitBoxOrient = 'vertical';
-            element.style.display = '-webkit-box';
-            element.style.webkitLineClamp = resultCountLines;
-        },
-        calcClamp: function () {
-            if (this.$wrapperSelector.length > 0) {
-                if (this.$wrapperSelector.children().length > 0) {
-                    let CountChildrenElements = this.$wrapperSelector.children();
+    //Инициализируем наш враппер и вызываем функцию подсчёта
+    constructor: function (wrapperSelector) {
+        this.$wrapperSelector = $(wrapperSelector);
+        ClampInner.calcClamp();
+    },
+    //Добавляем стили к элементу
+    addClampStyle: function (element, resultCountLines) {
+        element.style.overflow          = 'hidden';
+        element.style.textOverflow      = 'ellipsis';
+        element.style.webkitBoxOrient   = 'vertical';
+        element.style.display           = '-webkit-box';
+        element.style.webkitLineClamp   = resultCountLines;
+    },
+    //Считаем высоты дочерних элементов и применяем стили к дочернему элементу
+    calcClamp: function () {
+        if (this.$wrapperSelector.length > 0) {
+            if (this.$wrapperSelector.children().length > 0) {
+                let CountChildrenElements   = this.$wrapperSelector.children();
+                const wrapperHeight         = this.$wrapperSelector[0].clientHeight;
 
-                    const wrapperHeight = this.$wrapperSelector[0].clientHeight;
+                let counter         = 0,
+                    ChildrenHeight  = 0,
+                    allMargins      = 0;
 
-                    let counter = 0;
-                    let ChildrenHeight = 0;
-                    let allMargins = 0;
-                    CountChildrenElements.each(function() {
-                        let CurrentElement = CountChildrenElements[counter];
-                        let CurrentElementHeight = CountChildrenElements[counter].clientHeight;
-                        let currentElementMargin = parseInt(window.getComputedStyle(CountChildrenElements[counter]).marginBottom);
-                        allMargins += currentElementMargin;
-                        ChildrenHeight += CurrentElementHeight;
-                        if (CurrentElementHeight > wrapperHeight || ChildrenHeight > wrapperHeight) {
-                            const correctingPixels = ChildrenHeight - wrapperHeight;
-                            const elementHeight = CurrentElementHeight - correctingPixels - allMargins;
-                            const lineHeight = parseInt($(CountChildrenElements[counter]).css('line-height'));
-                            const countLines = CurrentElementHeight / lineHeight;
-                            let allLinesHeight = lineHeight * countLines;
+                CountChildrenElements.each(function() {
+                    let CurrentElement          = CountChildrenElements[counter],
+                        CurrentElementHeight    = CountChildrenElements[counter].clientHeight,
+                        currentElementMargin    = parseInt(window.getComputedStyle(CountChildrenElements[counter]).marginBottom);
 
-                            while (allLinesHeight > elementHeight) {
-                                allLinesHeight -= lineHeight;
-                            }
+                    allMargins      += currentElementMargin;
+                    ChildrenHeight  += CurrentElementHeight;
+                    if (CurrentElementHeight > wrapperHeight || ChildrenHeight > wrapperHeight) {
+                        const   correctingPixels    = ChildrenHeight - wrapperHeight,
+                            elementHeight       = CurrentElementHeight - correctingPixels - allMargins,
+                            lineHeight          = parseInt($(CountChildrenElements[counter]).css('line-height')),
+                            countLines          = CurrentElementHeight / lineHeight;
 
-                            const resultCountLines = allLinesHeight / lineHeight;
-                            ClampInner.addClampStyle(CurrentElement, resultCountLines)
+                        let allLinesHeight          = lineHeight * countLines;
+
+                        while (allLinesHeight > elementHeight) {
+                            allLinesHeight -= lineHeight;
                         }
-                        counter++;
-                    })
-                }
+
+                        const resultCountLines = allLinesHeight / lineHeight;
+                        ClampInner.addClampStyle(CurrentElement, resultCountLines)
+                    }
+                    counter++;
+                })
             }
         }
     }
+}
